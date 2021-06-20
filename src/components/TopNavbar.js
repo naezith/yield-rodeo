571ea9b9819939d5aa136083ac307fa9937c9b7e
naezith <naezith@users.noreply.github.com>
2021-06-21 01:12:17 +0300

    click to copy address

--- src/components/TopNavbar.js
+++ src/components/TopNavbar.js
@@ -1,23 +1,40 @@
-import {Form, Nav, Navbar, NavDropdown} from 'react-bootstrap'
+import {Nav, Navbar, OverlayTrigger, Tooltip} from 'react-bootstrap'
 
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 import './TopNavbar.scss'
+import {useState} from 'react'
 
-const TopNavbar = () =>
-  <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">
-      <img
-        alt=""
-        src="/hat_short.png"
-        className="navbar-image"
-      />{'   '}
-      yield.rodeo
-    </Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="ml-auto">
-      ❤️ 0x6e1b11ad54068dd3C7A2cAf334A7010f0B59511C ❤️
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
+const TopNavbar = () => {
+  const [copied, setCopied] = useState(false)
+  return (
+    <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src="/hat_short.png"
+            className="navbar-image"
+          />{'   '}
+          yield.rodeo
+        </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <OverlayTrigger
+            placement="bottom"
+            overlay={<Tooltip>{copied ? 'Copied!' : 'Click to copy!'}</Tooltip>}
+          >
+          <CopyToClipboard text={'0x6e1b11ad54068dd3C7A2cAf334A7010f0B59511C'}
+          onCopy={() => {
+            setCopied(true)
+            setTimeout(() => { setCopied(false) },1000)
+          }}>
+            <span>❤️ 0x6e1b11ad54068dd3C7A2cAf334A7010f0B59511C ❤</span>
+          </CopyToClipboard>
+        </OverlayTrigger>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
 
 export default TopNavbar
