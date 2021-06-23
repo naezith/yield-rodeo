291d19b5e19f97c7828a86cd97e09a3d59a7a838
naezith <naezith@users.noreply.github.com>
2021-06-23 16:53:49 +0300

    fix form params

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -29,14 +29,14 @@ const Filters = ({poolCount}) => {
       <Jumbotron className='filters'>
         <Form.Group>
           <Form.Label>💎 Coins to Ride</Form.Label>
-          <Form.Control name='desiredCoins' value={desiredCoins} defaultValue={desiredCoins} size="sm" type="text" placeholder="e.g. usdc dai busd" onChange={handleChange} />
+          <Form.Control name='desiredCoins' value={desiredCoins}size="sm" type="text" placeholder="e.g. usdc dai busd" onChange={handleChange} />
           <Form.Check>
-            <Form.Check.Input id='strictFilter' name='strictFilter' checked={strictFilter} defaultChecked={strictFilter} type="checkbox" onChange={handleCheckBoxChange} />
-            <Form.Check.Label for='strictFilter'>Show ONLY these coins <span className='silent'>(USDC won't show USDC-DAI)</span></Form.Check.Label>
+            <Form.Check.Input id='strictFilter' name='strictFilter' checked={strictFilter} type="checkbox" onChange={handleCheckBoxChange} />
+            <Form.Check.Label htmlFor='strictFilter'>Show ONLY these coins <span className='silent'>(USDC won't show USDC-DAI)</span></Form.Check.Label>
           </Form.Check>
           <Form.Check>
-            <Form.Check.Input id='exactMatch' name='exactMatch' checked={exactMatch} defaultChecked={exactMatch} type="checkbox" onChange={handleCheckBoxChange} />
-            <Form.Check.Label for='exactMatch'>Exact Match <span className='silent'>(USDT won't match fUSDT)</span></Form.Check.Label>
+            <Form.Check.Input id='exactMatch' name='exactMatch' checked={exactMatch} type="checkbox" onChange={handleCheckBoxChange} />
+            <Form.Check.Label htmlFor='exactMatch'>Exact Match <span className='silent'>(USDT won't match fUSDT)</span></Form.Check.Label>
           </Form.Check>
         </Form.Group>
 
@@ -44,12 +44,12 @@ const Filters = ({poolCount}) => {
 
         <Form.Group>
           <Form.Check>
-            <Form.Check.Input id='includeLPs' name='includeLPs' checked={includeLPs} defaultChecked={includeLPs} type="checkbox" onChange={handleCheckBoxChange} />
-            <Form.Check.Label for='includeLPs'>Include Liquidity Pools</Form.Check.Label>
+            <Form.Check.Input id='includeLPs' name='includeLPs' checked={includeLPs}  type="checkbox" onChange={handleCheckBoxChange} />
+            <Form.Check.Label htmlFor='includeLPs'>Include Liquidity Pools</Form.Check.Label>
           </Form.Check>
           <Form.Check>
-            <Form.Check.Input id='includeSingleAssets' name='includeSingleAssets' checked={includeSingleAssets} defaultChecked={includeSingleAssets} type="checkbox" onChange={handleCheckBoxChange} />
-            <Form.Check.Label for='includeSingleAssets'>Include Single Assets</Form.Check.Label>
+            <Form.Check.Input id='includeSingleAssets' name='includeSingleAssets' checked={includeSingleAssets} type="checkbox" onChange={handleCheckBoxChange} />
+            <Form.Check.Label htmlFor='includeSingleAssets'>Include Single Assets</Form.Check.Label>
           </Form.Check>
         </Form.Group>
 
@@ -57,7 +57,7 @@ const Filters = ({poolCount}) => {
 
         <Form.Group>
           <Form.Label>🏰 Platforms to Ride</Form.Label>
-          <Form.Control name='desiredPlatforms' value={desiredPlatforms} defaultValue={desiredPlatforms} size="sm" type="text" placeholder="e.g. cake aave sushi" onChange={handleChange}/>
+          <Form.Control name='desiredPlatforms' value={desiredPlatforms} size="sm" type="text" placeholder="e.g. cake aave sushi" onChange={handleChange}/>
         </Form.Group>
 
         <hr/>
