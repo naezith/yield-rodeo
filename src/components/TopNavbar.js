2ccd8f845a5b4e909622e9c7f49ec3d9bffb0a8a
naezith <naezith@users.noreply.github.com>
2021-12-09 15:09:39 +0300

    cleanup

--- src/components/TopNavbar.js
+++ src/components/TopNavbar.js
@@ -1,11 +1,11 @@
-import {Nav, Navbar, OverlayTrigger, Tooltip} from 'react-bootstrap'
+import {Nav, Navbar/*, OverlayTrigger, Tooltip*/} from 'react-bootstrap'
 
-import {CopyToClipboard} from 'react-copy-to-clipboard';
+// import {CopyToClipboard} from 'react-copy-to-clipboard';
 import './TopNavbar.scss'
-import {useState} from 'react'
+// import {useState} from 'react'
 
 const TopNavbar = () => {
-  const [copied, setCopied] = useState(false)
+  // const [copied, setCopied] = useState(false)
   return (
     <Navbar bg="dark" variant="dark">
         <Navbar.Brand href="">
@@ -17,18 +17,20 @@ const TopNavbar = () => {
           <span className='navbar-title'>yield.rodeo</span>
         </Navbar.Brand>
         <Nav className="ml-auto">
+        {/* Donation button
           <OverlayTrigger
             placement="bottom"
             overlay={<Tooltip>{copied ? 'Copied!' : 'Click to copy!'}</Tooltip>}
           >
-            <CopyToClipboard text={'0x6e1b11ad54068dd3C7A2cAf334A7010f0B59511C'}
+            <CopyToClipboard text={'0x00000000000000000000000'}
             onCopy={() => {
               setCopied(true)
               setTimeout(() => { setCopied(false) },1000)
             }}>
-              <span>❤️ Donate: 0x6e1b11ad54068dd3C7A2cAf334A7010f0B59511C ❤️</span>
+              <span>❤️ Donate: 0x00000000000000000000000 ❤️</span>
             </CopyToClipboard>
           </OverlayTrigger>
+          */}
         </Nav>
       <Nav>
         <Nav.Link className='navbar-contact' href={'https://naezith.com/?id=contact'} target="_blank" rel="noreferrer">Contact</Nav.Link>
